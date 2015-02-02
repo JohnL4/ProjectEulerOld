4631480f8aa23d7bf35d38ec7cc2981fa4571ed8
John Lusk <john-git@how-hard-can-it-be.com>
2015-02-01 20:08:25 -0500

    Initial commit of parent POM.

---
+++ src/main/java/com/how_hard_can_it_be/App.java
@@ -0,0 +1,13 @@
+package com.how_hard_can_it_be;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
